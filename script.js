92aa71055f0f8a462ba6a5f7bdd029f8876f3cb3
MarcosvBueno <mvbueno07@gmail.com>
2023-02-15 22:39:21 -0300

    adiciona funcao deleteTask e localStorage

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let tasks = [];
+let tasks = JSON.parse(localStorage.getItem("@to-do-list")) || [];
 let body = document.body;
 const maxNumber = 5;
+
 document.addEventListener('keyup', function addTodoTask(e) {
   let inputTodo = document.getElementById('input-todo').value;
 
@@ -20,20 +21,25 @@ document.addEventListener('keyup', function addTodoTask(e) {
         tasks.push(inputTodo);
         console.log(tasks);
         renderTasks();
+        saveData();
       } else {
         alert('Task already exists');
       }
    
   } 
+  saveData();
   
 });
+
 
 function renderTasks() {
   let conteudo = document.querySelector('.conteudo');
   conteudo.innerHTML = ''; // limpa o conteúdo atual para renderizar as tarefas sem repetir
-
-  tasks.map(todo => { // map function pra utilizar todos os dados guardados no array principal e retornalos no "todo"
+  
+  tasks.map((todo) => { // map function pra utilizar todos os dados guardados no array principal e retornalos no "todo"
     document.getElementById('input-todo').value = '';
+
+
 
     // nova div-container para cada tarefa
     let divContainer = document.createElement('div');
@@ -46,25 +52,35 @@ function renderTasks() {
     input.className = 'input-group';
     input.id = 'input-id'
 
+
+    let icon = document.createElement('i');
+    icon.className = "fa-solid fa-trash icon hide-icon";
+    icon.id = 'icon';
+    icon.setAttribute('onclick', `deleteTask(this.parentNode)`); //atribuo a função ao meu icone (trash);
+
     // criando texto que vai servir de label para nosso input
     let p = document.createElement('p');
     p.textContent = todo;
+
 
     // funcao que adiciona o estilo line-thourgh ao texto(p) do input
     input.addEventListener("change", function() {
       if (this.checked) {
         p.style.textDecoration = "line-through";
+
       } else {
         p.style.textDecoration = "none";
+
       }
+      saveData();
     });
 
     // adiciono os elementos criados a div-container
     divContainer.appendChild(input);
     divContainer.appendChild(p);
+    divContainer.appendChild(icon);
 
     // Adicione a nova div-container à div conteúdo
-    let conteudo = document.querySelector('.conteudo');
     conteudo.appendChild(divContainer);
 
     // tira o display none da div-container que está vazia
@@ -74,8 +90,12 @@ function renderTasks() {
     // adiciona a quantidade de itens a cada vez que é adicionado um 
     let total = document.getElementById('total-todo');
     total.textContent = tasks.length + ' items';
+
+    
   });
 }
+
+
 
 let clearALL = document.getElementById('clear-todo');
 clearALL.addEventListener('click', function () {
@@ -84,6 +104,7 @@ clearALL.addEventListener('click', function () {
   tasks = []; //zerando array 
   let total = document.getElementById('total-todo');
   total.textContent = '0 items'; //zerando quantidade de items dentro do array pai
+  saveData();
 });
 
 
@@ -98,13 +119,24 @@ showCompleted.addEventListener('click', function () {
   let isChecked = false; // variável de controle
 
   inputs.map((inputMap,i ) => { //metodo map passando o elemento e o index
+    let icons = [...document.querySelectorAll('#icon')];
     if (inputMap.checked) { //input marcado 
+      
       isChecked = true;
       divContainers[i].style.display = 'flex';
       soma++; 
       total.textContent = soma + " items";
+      icons.map(icon => {
+        icon.style.display = 'none';
+        return ;
+      });
+
     } else {
       divContainers[i].style.display = 'none';
+      icons.map(icon => {
+        icon.style.display = 'flex';
+        return ;
+      });
     }
   });
 
@@ -125,17 +157,22 @@ showCompleted.addEventListener('click', function () {
      }
      return;
    }
-
+   
 
 });
 
 let showAll = document.getElementById('showall');
 showAll.addEventListener('click', function () {
   let divContainers = [...document.querySelectorAll('#id-container')];
+  let icons = [...document.querySelectorAll('#icon')];
   let total = document.getElementById('total-todo');
   total.textContent = divContainers.length + " items";
   divContainers.map(div => {  //usando a função map para retornar todos as divs
     div.style.display = 'flex';
+    icons.map(icon => {
+      icon.style.display = 'flex';
+      return false;
+    });
   });
 
   let removeDivContainer = document.querySelector('#remove-container');
@@ -152,13 +189,21 @@ let showActive = document.getElementById('showActive');
 showActive.addEventListener('click',function() {
   let inputs = [...document.querySelectorAll('#input-id')]; //transformos os meus inputs em array
   let divContainers = [...document.querySelectorAll('#id-container')]; //transformos os minhas div's em array
+  let icons = [...document.querySelectorAll('#icon')];
   let total = document.getElementById('total-todo');
   let soma = 0;
   let divRemove = document.getElementById('remove-container');
+
   const exist = document.body.contains(divRemove);
+
   inputs.map((inputMap , i) =>{ //metodo map passando o elemento e o index
     if (!inputMap.checked) { //se o input não estiver marcado
       divContainers[i].style.display = 'flex';
+      total.textContent = divContainers.length + " Items";
+      icons.map(icon => {
+        icon.style.display = 'none';
+        return false;
+      });
       if(exist){
         divRemove.style.display = 'none';
       }
@@ -168,7 +213,24 @@ showActive.addEventListener('click',function() {
     } else {
       divContainers[i].style.display = 'none';
     }
-
+    
   });
 });
 
+function deleteTask(el) {
+  let container = el.closest('.div-container');
+  let p = container.querySelector('p');
+  let index = tasks.indexOf(p.textContent);
+  tasks.splice(index, 1); // Remove a tarefa correspondente do array tasks
+  container.remove();
+
+  let totalTodo = document.getElementById('total-todo');
+  totalTodo.textContent = tasks.length + ' items';
+  saveData();
+}
+
+function saveData() {
+  localStorage.setItem("@to-do-list", JSON.stringify(tasks));
+}
+
+renderTasks();
